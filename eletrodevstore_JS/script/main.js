bac9ca18f72dd3c0e8b75f2d39764aab09e6cce8
Marcos Antonio <marcos-rj@outlook.com>
2020-10-15 16:08:10 -0300

    cartCount added some css adjusts and js stuff

--- eletrodevstore_JS/script/main.js
+++ eletrodevstore_JS/script/main.js
@@ -1,6 +1,5 @@
 
-// função que ativa através do onclick
-
+// função p/ filtrar os produtos por categorias
 function showProducts(categories) {
     let elements = document.getElementsByClassName('productCards'); //Pegar todas as categorias(id) na classe
 
@@ -26,8 +25,74 @@ let showAllProducts = () => {
 // Função para adicionar produtos no carrinho
 function cartUpValue() {
     const cartCount = document.querySelector('.cartCount')
-    cartCount.innerText="1";
+    let count = sessionStorage.getItem('contador') == null || undefined ? 0 : sessionStorage.getItem('contador')
+    count++;
+    cartCount.innerHTML = count;
+    sessionStorage.setItem('contador', count)
+
+    if(count > 0) {
+        cartCount.style = "transition: 1s; background: #773adc;"
+    }
 }
 
-//Ainda trabalhando, valor na span muda com innertext, porém precisa
-// de uma lógica mais elaborada, para que o incremento seja automático.
+// Limpar carrinho
+function deleteCart() {
+    const cartCount = document.querySelector('.cartCount')
+    let apagar = confirm("Deseja apagar o carrinho?");
+
+    if (apagar){
+        cartCount.innerHTML = 0;
+        sessionStorage.setItem('contador', 0);
+    }
+}
+
+//Guardar valor do carrinho, quando mudar ou atualizar página
+document.addEventListener("DOMContentLoaded", (event) => {
+    function getCartValue() {
+        const cartCount = document.querySelector('.cartCount')
+        let count = sessionStorage.getItem('contador') == null || undefined ? 0 : sessionStorage.getItem('contador')
+        cartCount.innerHTML = count;
+
+        if(count > 0) {
+            cartCount.style = "transition: 1s; background: #773adc"
+        }
+    }
+      getCartValue()
+  });
+
+
+/*
+// Função para dar destaque na imagem
+var modal = document.getElementsByClassName("modal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var image = document.getElementsByClassName('image')
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+function showIn(el) {
+    var imageSource = el.src;
+    console.log(el.src)
+    var altText = el.alt;
+    console.log(el.alt)
+
+    modal.style = "visibility: visible";
+    modalImg.src = imageSource;
+    console.log(imgSrc)
+    captionText.innerHTML = altText;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+function closeButton(el) { 
+  modal.style.display = "none";
+  console.log("ok")
+}
+
+window.onclick = (event) => {
+  if(event.target == modal) {
+    modal.style.display = "none";
+  }
+} */
