2a7ebdbe57bd0c67afe1758dbc5afb44edc37348
Marcos Antonio <marcos-rj@outlook.com>
2020-11-03 12:59:59 -0300

    videoGames class removed

--- eletrodevstore_PHP/config/products.php
+++ eletrodevstore_PHP/config/products.php
@@ -11,7 +11,7 @@
           $preco_novo = $rows['preco_novo'];
           $alt_imagem = $rows['alt_imagem'];
 ?>
-  <div class="productCards videoGames" id="<?php echo $categoria; ?>">
+  <div class="productCards" id="<?php echo $categoria; ?>">
       <img class="image" src="<?php echo $imagem; ?>" alt="<?php echo $alt_imagem?>" onclick="showIn(this)">
       <h3><?php echo $descricao; ?></h3>
       <p class="oldPrice">R$ <?php echo number_format($preco_antigo, 2, ",", "."); ?></p>
